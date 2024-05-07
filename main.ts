21ab50f15b3e6b5802d933e2474e3d7684f3525f
Ahmadullah Naibi <ahnai17@student.sdu.dk>
2024-05-07 14:47:15 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,43 @@
+radio.setGroup(2)
+basic.forever(function () {
+    if (gamerbit.isPressed(GamerBitPin.P16)) {
+        if (gamerbit.isPressed(GamerBitPin.P0)) {
+            radio.sendNumber(0)
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                # . # . #
+                . . # . .
+                . . # . .
+                `)
+        } else if (gamerbit.isPressed(GamerBitPin.P2)) {
+            radio.sendNumber(1)
+            basic.showLeds(`
+                . # # # #
+                . . . # #
+                . . # . #
+                . # . . #
+                . # . . .
+                `)
+        } else if (gamerbit.isPressed(GamerBitPin.P8)) {
+            radio.sendNumber(2)
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # . # . #
+                . # # # .
+                . . # . .
+                `)
+        } else if (gamerbit.isPressed(GamerBitPin.P1)) {
+            radio.sendNumber(3)
+            basic.showLeds(`
+                # # # # .
+                # # . . .
+                # . # . .
+                # . . # .
+                . . . # .
+                `)
+        }
+    }
+    basic.clearScreen()
+})
