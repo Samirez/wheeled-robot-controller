acd3a61acbb2b240985996474bae3fa96cc55697
Ahmadullah Naibi <ahnai17@student.sdu.dk>
2024-05-12 13:36:40 +0000

    optimized code to make more clear

--- main.ts
+++ main.ts
@@ -1,18 +1,7 @@
-gamerbit.onEvent(GamerBitPin.P12, GamerBitEvent.Down, function () {
-    radio.sendNumber(4)
-    music.play(music.createSoundExpression(
-    WaveShape.Sine,
-    5000,
-    0,
-    255,
-    0,
-    2000,
-    SoundExpressionEffect.None,
-    InterpolationCurve.Linear
-    ), music.PlaybackMode.UntilDone)
-})
 gamerbit.onEvent(GamerBitPin.P16, GamerBitEvent.Down, function () {
-    music.play(music.stringPlayable("C5 A - F - E D C ", 180), music.PlaybackMode.UntilDone)
+    while (gamerbit.isPressed(GamerBitPin.P16)) {
+        music.play(music.stringPlayable("C5 A - F - E D C ", 180), music.PlaybackMode.UntilDone)
+    }
 })
 radio.setGroup(2)
 basic.forever(function () {
@@ -54,6 +43,18 @@ basic.forever(function () {
                 . . . # .
                 `)
         }
+    } else if (gamerbit.isPressed(GamerBitPin.P12)) {
+        radio.sendNumber(4)
+        music.play(music.createSoundExpression(
+        WaveShape.Sine,
+        5000,
+        0,
+        255,
+        0,
+        2000,
+        SoundExpressionEffect.None,
+        InterpolationCurve.Linear
+        ), music.PlaybackMode.UntilDone)
     }
     basic.clearScreen()
 })
